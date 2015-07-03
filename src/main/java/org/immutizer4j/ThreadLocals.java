dc2b1df48d7d636c23d78456f06633f12857ae23
Jacek Furmankiewicz <jacek99@gmail.com>
2015-07-02 23:10:06 -0500

    got rid of compilation warning

--- src/main/java/org/immutizer4j/ThreadLocals.java
+++ src/main/java/org/immutizer4j/ThreadLocals.java
@@ -1,6 +1,5 @@
 package org.immutizer4j;
 
-import java.util.StringJoiner;
 
 /**
  * Internal thread local variables for max performance
@@ -13,15 +12,15 @@ class ThreadLocals {
      * Thread-local StringBuilder, avoids the overhead of instantiating a new one every time
      * It gets reset upon every get() operation
      */
-    public static final ThreadLocal<StringBuilder> STRINGBUILDER = new ThreadLocal() {
+    public static final ThreadLocal<StringBuilder> STRINGBUILDER = new ThreadLocal<StringBuilder>() {
         @Override
         public StringBuilder initialValue() {
             return new StringBuilder(50);
         }
 
         @Override
-        public Object get() {
-            StringBuilder bld = (StringBuilder) super.get();
+        public StringBuilder get() {
+            StringBuilder bld = super.get();
             bld.setLength(0);
             return bld;
         }
